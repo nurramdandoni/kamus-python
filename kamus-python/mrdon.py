08e9a6cd9c02258b85f9f50cd876536f6c43c58a
Doni Nurramdan <33200117+nurramdandoni@users.noreply.github.com>
2018-07-18 12:51:04 +0700

    mrdon-v1.0
    firmware Core intruction Artificial Intelegence

---
+++ kamus-python/mrdon.py
@@ -0,0 +1,10 @@
+	
+# untuk pengembangan input adalah SpeechRecognize
+a=input() 
+
+# pegembangan bisa untuk perintah home automation/ robotika
+dict = {'Saya': 'i, Iam, me', 'Kamu': 'you, your'}
+
+# untuk pengembangan bisa output ke arduino melalui serial
+# dan mencari kata kerja pada sebuah kalimat
+print ("Hasil : ", dict[a])
